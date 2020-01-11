8a70aaa4d9ee911fd8bb68f6969d93362c393f63
Medi Risasi <morisasy@gmail.com>
2020-01-11 23:44:28 +0200

    bloglist frontend, step2 & step3

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -148,6 +148,9 @@ const handleLogout = async (event) => {
                           url={url}
                           handleUrlChange={handleUrlChange}               
                         />
+                        {blogs.map(blog =>
+                          <Blog key={blog.id} blog={blog} />
+                        )}
                       </div>
                 }
                
